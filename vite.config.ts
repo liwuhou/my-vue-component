a30f51d142fbc3de6b5b0dd87b123d2e005ee17b
阿五 <hugewilliam@foxmail.com>
2022-08-29 22:32:12 +0800

    chore: add some config for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -4,49 +4,49 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from './config/unocss'
 import { resolve, dirname } from 'node:path'
-import { fileURLToPath} from 'node:url'
+import { fileURLToPath } from 'node:url'
 
 const currFilePath = dirname(fileURLToPath(import.meta.url))
 
 const rollupOptions = {
-    external: ['vue', 'vue-router'],
-    output: {
-        globals: {
-            vue: 'vue'
-        }
-    }
+  external: ['vue', 'vue-router'],
+  output: {
+    globals: {
+      vue: 'vue',
+    },
+  },
 }
 
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueJsx(),
-        Unocss(),
-    ],
-    // https://vitejs.dev/config/
-    test: {
-        globals: true,
-        environment: 'happy-dom',
-        transformMode: {
-            web: [/.[tj]sx$/]
-        }
+  plugins: [vue(), vueJsx(), Unocss()],
+  // https://vitejs.dev/config/
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    transformMode: {
+      web: [/.[tj]sx$/],
     },
-    resolve: {
-        alias: {
-            '@': resolve(currFilePath, 'src'),
-            '@utils': resolve(currFilePath, 'src/utils'),
-            '@test': resolve(currFilePath, 'src/utils/test')
-        }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(currFilePath, 'src'),
+      '@utils': resolve(currFilePath, 'src/utils'),
+      '@test': resolve(currFilePath, 'src/utils/test'),
     },
-    build: {
-        rollupOptions,
-        minify: false,
-        lib: {
-            entry: './src/entry.ts',
-            name: 'SmartyUI',
-            fileName: 'smarty-ui',
-            // 导出模块格式
-            formats: ['es', 'umd', 'iife', 'cjs']
-        }
-    }
+  },
+  build: {
+    rollupOptions,
+    minify: 'terser',
+    sourcemap: true,
+    // brotliSize: true,
+    cssCodeSplit: true,
+    emptyOutDir: true,
+    lib: {
+      entry: './src/entry.ts',
+      name: 'SmartyUI',
+      fileName: 'smarty-ui',
+      // 导出模块格式
+      formats: ['es', 'umd', 'iife', 'cjs'],
+    },
+  },
 })
